9eeae23a093f0aa33e03051e48318b6886e0e24d
Stuart McCord <stuart.mccord@gmail.com>
2020-06-21 23:38:34 +0100

    basic options for bbc good food parser

--- mozerella/parser.py
+++ mozerella/parser.py
@@ -1,4 +1,6 @@
 import re
+import urllib3
+from bs4 import BeautifulSoup
 
 
 class Parser(object):
@@ -14,8 +16,44 @@ class Parser(object):
         extract_domain_pattern = "(?:(?:https?|ftp)://)?(?:www\.)?([^/\r\n]+)(?:/[^\r\n]*)?"
         return re.search(extract_domain_pattern, url).groups()[0]
 
-
-class BBCGoodFoodParser(Parser):
-
     def __init__(self, url):
         self.url = url
+        self.document = self.get_document()
+
+    def get_document(self):
+        http = urllib3.PoolManager()
+        return http.urlopen(self.url)
+
+
+class BBCGoodFoodParser(Parser):
+    def __init__(self, url):
+        super().__init__(url)
+        self.parser_engine = None
+
+    def get_parser_engine(self):
+        if self.parser_engine:
+            return self.parser_engine
+
+        return BeautifulSoup(self.document, 'html.parser')
+
+    def get_title(self):
+        return self.get_parser_engine().select('h1.recipe-header__title')[0].get_text()
+
+    def get_description(self):
+        return self.get_parser_engine().select('.recipe-header__description p')[0].get_text()
+
+    def get_prep_time(self):
+        return self.get_parser_engine().select('.recipe-details__cooking-time-prep .mins')[0].get_text()
+
+    def get_cook_time(self):
+        return self.get_parser_engine().select('.recipe-details__cooking-time-cook .mins')[0].get_text()
+
+    def get_servings(self):
+        return self.get_parser_engine().select('.recipe-details__item--servings .recipe-details__text')[0].get_text().rstrip().lstrip()
+
+    def get_image(self):
+        return self.get_parser_engine().select('.recipe-header .img-container img')[0]['src']
+
+    def get_directions(self):
+        method = self.get_parser_engine().select('#recipe-method ol.method__list li.method__item > p')
+        return [item.get_text().lstrip().rstrip() for item in method]
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,15 +1,60 @@
-from mozerella.parser import Parser
+from mozerella.parser import Parser, BBCGoodFoodParser
+import pytest
+import os
 
 
 class TestParser:
-    def test_get_domain(self):
-        domain = Parser.get_domain('https://www.bbcgoodfood.com/recipes/cinnamon-rolls')
+    def test_get_domain(self, bbc_good_food_url):
+        domain = Parser.get_domain(bbc_good_food_url)
         assert domain == 'bbcgoodfood.com'
 
-    def test_bbc_food(self, mocker):
-        url = "https://www.bbcgoodfood.com/recipes/cinnamon-rolls"
-        mock = mocker.patch('mozerella.parser.BBCGoodFoodParser.__init__', return_value=None)
-        Parser.factory(url)
-        mock.assert_called_once_with(url)
+    def test_get_document(self, mocker, bbc_good_food_url):
+        mock_requests_get = mocker.patch('urllib3.PoolManager.urlopen', return_value="<html></html>")
+        Parser.factory(bbc_good_food_url)
+
+        mock_requests_get.assert_called_once_with(bbc_good_food_url)
+
+    def test_bbc_food(self, mocker, bbc_good_food_url):
+        mock_init = mocker.patch('mozerella.parser.BBCGoodFoodParser.__init__', return_value=None)
+        Parser.factory(bbc_good_food_url)
+
+        mock_init.assert_called_once_with(bbc_good_food_url)
 
 
+class TestBBCGoodFoodParser:
+    def test_it_parses_the_document(self, bbc_good_food_parser):
+        assert bbc_good_food_parser.get_title() == "Cinnamon rolls"
+        assert bbc_good_food_parser.get_description() == "Try these easy cinnamon rolls with vanilla icing as a treat for afternoon tea. They're delicious served warm or cold – you can also make them ahead and freeze"
+        assert bbc_good_food_parser.get_prep_time() == "40 mins"
+        assert bbc_good_food_parser.get_cook_time() == "35 mins"
+        assert bbc_good_food_parser.get_servings() == 'Makes 8'
+        assert bbc_good_food_parser.get_image() == '//www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2018/06/cinnamon-buns.jpg?itok=N8YEo0yS'
+
+    def test_it_parses_the_directions(self, bbc_good_food_parser):
+        directions = bbc_good_food_parser.get_directions()
+        assert len(directions) == 3
+        assert "Heat oven to 180C/fan 160C/gas 4." in directions[0]
+        assert "Mix the filling ingredients together." in directions[1]
+        assert "Sift the icing sugar into a large bowl" in directions[2]
+
+
+@pytest.fixture
+def bbc_good_food_parser(bbc_good_food_document, bbc_good_food_url, mocker):
+    mocker.patch('mozerella.parser.BBCGoodFoodParser.get_document', return_value=bbc_good_food_document)
+    parser = BBCGoodFoodParser(bbc_good_food_url)
+    return parser
+
+
+@pytest.fixture
+def bbc_good_food_url():
+    return "https://www.bbcgoodfood.com/recipes/cinnamon-rolls"
+
+
+@pytest.fixture
+def bbc_good_food_document():
+    dir = os.path.dirname(__file__)
+    relative_file_path = "fixtures/bbcgoodfood.html"
+    absolute_file_path = os.path.join(dir, relative_file_path)
+
+    document = open(absolute_file_path, "r")
+    return document.read()
